c2e4a119af8e6a50ae18e362fa562a01b7ffabfa
Tarcisio <tleite256@gmail.com>
2022-11-17 17:39:20 -0300

    Atualização Classe Assinante

--- src/Assinante.java
+++ src/Assinante.java
@@ -1,18 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Assinante {
+
+	private long cpf;
+	private String nome;
+	private int numero;
 	protected Chamada[] chamadas;
 	protected Integer numChamadas;
-	
+
 	public Assinante(Integer numero) {
 		this.chamadas = new Chamada[numero];
 		this.numChamadas = numero;
 	}
 	
+	public long getCpf() {
+		
+		return this.cpf;
+	}
 	
-
-	
-	
-	
+	public String toString()
+	{
+		return "Nome: " + this.nome + "; Cpf: " + this.cpf + "; Numero: " + this.numero + "; Chamadas: " + this.chamadas + "; Número de chamadas: " + this.numChamadas;
+		
+	}
 }
